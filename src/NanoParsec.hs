824bcbd4fb1c0321739798bc84f4332cb774244b
Rick Winfrey <rewinfrey@github.com>
2018-09-07 17:06:35 -0700

    Fix typos.

--- src/NanoParsec.hs
+++ src/NanoParsec.hs
@@ -9,9 +9,10 @@ module NanoParsec where
   a parser and whose return value is a new parser.
 
   A parser is a function that accepts String input and whose result value is structured
-  to indicate where parsing succeeded, stopped, and what if any of the input String is remaining for a subsequent parser.
+  to indicate where parsing succeeded, stopped, and what if any of the input String
+  is remaining for a subsequent parser.
 
-  Parse combinators then are a collection of higher-order functions whose input
+  Parser combinators then are a collection of higher-order functions whose input
   are parsers and whose return values are also parsers. This allows for the composition
   of multiple parsers (higher-order functions) when consuming an input String.
   This approach is convenient for many reasons, but chief among them is the ability
